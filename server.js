365adca2143333ecd8e43dccc698dfa6e788c814
Benjamin Tan <demoneaux@gmail.com>
2014-04-04 10:45:21 +0800

    Update node files

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var PORT = Number(process.env.PORT)  || 8080;
 
-var http = require("http");
+var http = require('http'),
+    parseURL = require('url').parse,
+    parseQS = require('qs').parse;
 
-var pkg = require('lodash-cli/package.json');
-var bin = pkg.bin.lodash;
-var builder = require.resolve('lodash-cli/' + bin);
+var pkg = require('lodash-cli/package.json'),
+    bin = pkg.bin.lodash,
+    builder = require.resolve('lodash-cli/' + bin);
 
 var ecstatic = require('ecstatic');
+
+var mount = ecstatic({ });
 
 http.createServer(function(req, res) {
 
 }).listen(PORT);
-
